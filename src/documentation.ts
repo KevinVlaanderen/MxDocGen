9152a28da32350db4da9c43f78a63747a154594e
Kevin Vlaanderen <kevin.vlaanderen@mendix.com>
2020-02-19 09:54:59 +0100

    Extract filter functions

--- src/documentation.ts
+++ src/documentation.ts
@@ -1,24 +1,48 @@
-import {getFilteredDocuments, getFilteredModules, logProjectStructure, ProjectStructure} from "./projectStructure";
+import {
+    DocumentDescription,
+    getDocuments,
+    getModules,
+    logProjectStructure,
+    ProjectStructure
+} from "./projectStructure";
 import {MendixSdkClient} from "mendixplatformsdk";
 import {createWorkingCopy, WorkingCopyConfig} from "./sdk";
+import {DocumentType} from "./documentTypes";
+import ignore from "ignore";
 
 interface GenerateDocumentationConfig extends WorkingCopyConfig {
     modulesRegex: string;
     ignorePatterns: string[];
+    types: {
+        [type in DocumentType]: boolean;
+    }
 }
+
+const matchesRegex = (value: string, filter?: RegExp): boolean => filter ? value.match(filter) !== undefined : true;
+
+const createDocumentIgnoreFilter = (ignorePatterns?: string[]): (document: DocumentDescription) => boolean => {
+    const ig = ignore().add(ignorePatterns ?? []);
+    return (document: DocumentDescription) => {
+        const path = document.folder ? [document.folder, document.name].join("/") : document.name;
+        return !ig.ignores(path);
+    };
+};
 
 export const generateDocumentation = async (client: MendixSdkClient, config: GenerateDocumentationConfig): Promise<void> => {
     const model = await createWorkingCopy(client, config);
 
-    const projectStructure = getFilteredModules(model, new RegExp(config.modulesRegex))
-        .map(module => ({
-            name: module.name,
-            documents: getFilteredDocuments(module, config.ignorePatterns)
-        }))
-        .reduce((previousValue, currentValue) => {
-            previousValue[currentValue.name] = currentValue.documents;
-            return previousValue;
-        }, {} as ProjectStructure);
+    const projectStructure =
+        getModules(model)
+            .filter(module => matchesRegex(module.name, new RegExp(config.modulesRegex)))
+            .map(module => ({
+                name: module.name,
+                documents: getDocuments(module)
+                    .filter(createDocumentIgnoreFilter(config.ignorePatterns))
+            }))
+            .reduce((previousValue, currentValue) => {
+                previousValue[currentValue.name] = currentValue.documents;
+                return previousValue;
+            }, {} as ProjectStructure);
 
     logProjectStructure(projectStructure);
 
--- src/projectStructure.ts
+++ src/projectStructure.ts
@@ -14,6 +14,9 @@ export interface DocumentDescription {
     document: IDocument;
 }
 
+export const getModules = (model: IModel): IModule[] => model.allModules();
+export const getDocuments = (module: IModule): DocumentDescription[] => getDocumentDescriptions(module);
+
 export const logProjectStructure = (projectStructure: ProjectStructure) =>
     Object.keys(projectStructure).forEach(moduleName => {
         console.debug(moduleName);
@@ -25,30 +28,6 @@ export const logProjectStructure = (projectStructure: ProjectStructure) =>
             console.debug(`   ${path}`);
         })
     });
-
-export const getModules = (model: IModel): IModule[] => model.allModules();
-
-export const getFilteredModules = (model: IModel, filter?: RegExp): IModule[] =>
-    getModules(model)
-        .filter(module => filter ? module.name.match(filter) : true);
-
-export const getDocuments = (module: IModule): DocumentDescription[] => getDocumentDescriptions(module);
-
-export const getFilteredDocuments = (module: IModule, ignorePatterns?: string[]): DocumentDescription[] => {
-    const documents = getDocuments(module);
-
-    if (ignorePatterns) {
-        const filterFn = ignore().add(ignorePatterns).createFilter();
-        return documents.filter(documentDescription => {
-            const path = documentDescription.folder
-                ? [documentDescription.folder, documentDescription.name].join("/")
-                : documentDescription.name;
-            return filterFn(path);
-        });
-    }
-
-    return documents;
-};
 
 const getDocumentDescriptions = (folderBase: IFolderBase, path?: string): Array<DocumentDescription> =>
     [
